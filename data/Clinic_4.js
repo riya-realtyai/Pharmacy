1e97100a11eada0a07a7d3ae0ad1ef0f707c3dfb
riya-realtyai <108445694+riya-realtyai@users.noreply.github.com>
2023-10-04 16:41:55 +0530

    Add files via upload

---
+++ data/Clinic_4.js
@@ -0,0 +1,1 @@
+var json_Clinic_4 = {"type":"FeatureCollection","name":"Clinic_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/UniqueHospitalOnly_web_3.js
@@ -0,0 +1,1 @@
+var json_UniqueHospitalOnly_web_3 = {"type":"FeatureCollection","name":"UniqueHospitalOnly_web_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
